193a4fd2077a1eb03f7756e0b772306eaaf29d35
Samuel-Oli-P <samuel.pereira@sptech.school>
2023-06-06 17:23:31 -0300

    Feat: cli individual new

--- projeto-individual-cli-samuel/src/main/java/testLog/Teste.java
+++ projeto-individual-cli-samuel/src/main/java/testLog/Teste.java
@@ -1,8 +1,10 @@
+package testLog;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package testLog;
+
 
 import persistence.API;
 import persistence.Conexao;
